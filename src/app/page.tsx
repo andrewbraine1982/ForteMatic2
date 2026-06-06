ddb1d3d60713ffa94fd934a85209b055da2cfb83
andrewbraine1982 <andrewbraine@gmail.com>
2026-06-06 20:55:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,33 @@ export default function Home() {
           />
         </div>
       </section>
+      <section>
+  <h2>Targeted Support For Your Health Goals</h2>
+</section>
+
+<section>
+  <h2>As Seen In</h2>
+</section>
+
+<section>
+  <h2>Why 60,000+ Customers Choose ForteMatic</h2>
+</section>
+
+<section>
+  <h2>The Best GLP-1 Supplement</h2>
+</section>
+
+<section>
+  <h2>Explore The ForteMatic Family</h2>
+</section>
+
+<section>
+  <h2>Frequently Asked Questions</h2>
+</section>
+
+<section>
+  <h2>Stay In The Loop</h2>
+</section>
     </main>
   );
 }
