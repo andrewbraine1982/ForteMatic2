83e76385f6d277121deacd4975053a77f36c83f1
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-23 17:02:06 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,6 +105,29 @@ export default function Home() {
         <strong>Healthline</strong>
         <strong>mindbodygreen</strong>
       </section>
+      <section className="statsBar">
+
+  <div className="statCard">
+    <h3>60,000+</h3>
+    <p>Happy Customers</p>
+  </div>
+
+  <div className="statCard">
+    <h3>10,000+</h3>
+    <p>5★ Reviews</p>
+  </div>
+
+  <div className="statCard">
+    <h3>20+</h3>
+    <p>Countries Served</p>
+  </div>
+
+  <div className="statCard">
+    <h3>30-Day</h3>
+    <p>Money Back Guarantee</p>
+  </div>
+
+</section>
 
       <section id="goals" className="contentSection">
         <p className="sectionLabel">Health Goals</p>
