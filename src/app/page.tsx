47f9cf1b20d102fc6830e9f7104fa0a0ed4cb26b
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-24 14:29:24 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 import { useEffect, useRef, useState } from "react";
 import ClosingExperience from "./components/ClosingExperience";
@@ -90,74 +91,7 @@ const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 <Header amazonStoreUrl={amazonStoreUrl} />
 
-      <section className="hero">
-        <div className="heroLeft">
-          <div className="stars">
-            <span className="greenStars">★★★★★</span>
-            <span>60,000+ happy customers</span>
-          </div>
-
-          <h1>
-            Elevate your everyday <span className="italicWord">health.</span>
-          </h1>
-
-          <p>
-            Premium daily supplements designed to support GLP-1 support, gut
-            health, metabolism, energy, hormone support and long-term wellbeing.
-          </p>
-
-          <div className="heroButtons">
-            <a
-              className="primaryBtn"
-              href={amazonStoreUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>Shop on</span>
-
-              <span className="amazonLogoText">
-                amazon
-                <span className="amazonSmile"></span>
-              </span>
-            </a>
-
-            <a className="secondaryBtn" href="#products">
-              Explore Products
-            </a>
-          </div>
-
-          <ul className="trustRow">
-            <li className="trustItem">
-              <span>🌱</span>
-              <p>Premium Ingredients</p>
-            </li>
-
-            <li className="trustItem">
-              <span>🧪</span>
-              <p>Science Backed</p>
-            </li>
-
-            <li className="trustItem">
-              <span>🇺🇸</span>
-              <p>Made in USA</p>
-            </li>
-
-            <li className="trustItem">
-              <span>✅</span>
-              <p>GMP Certified</p>
-            </li>
-          </ul>
-        </div>
-
-        <div className="heroRight">
-          <img
-            src="/hero-clean.png"
-            alt="ForteMatic supplements"
-            className="heroImage"
-          />
-        </div>
-      </section>
-
+   <Hero amazonStoreUrl={amazonStoreUrl} />
       <section className="seenIn">
         <span>AS SEEN IN</span>
         <strong>Forbes</strong>
