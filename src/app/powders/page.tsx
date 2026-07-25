3a6be9e2c7abed53995940aa768f5c79fc548d2d
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-25 19:05:13 +0200

    Create page.tsx

---
+++ src/app/powders/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import Header from "../components/Header";
+const powders = [
+  {
+    name: "Creatine",
+    subtitle: "Micronised Monohydrate",
+    image: "/fortematic_creatine.png",
+    status: "In Development",
+  },
+  {
+    name: "Whey Protein",
+    subtitle: "Premium Protein Isolate",
+    image: "/fortematic_whey.png",
+    status: "In Development",
+  },
+  {
+    name: "Marine Collagen",
+    subtitle: "Hydrolysed Peptides",
+    image: "/fortematic_collagen.png",
+    status: "In Development",
+  },
+  {
+    name: "Daily Greens",
+    subtitle: "Superfood Blend",
+    image: "/fortematic_green.png",
+    status: "In Development",
+  },
+  {
+    name: "Electrolytes",
+    subtitle: "Hydration Formula",
+    image: "/fortematic_electrolytes.png",
+    status: "In Development",
+  },
+];
