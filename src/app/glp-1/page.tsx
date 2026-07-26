9506dfe1a4001c4b04c43bedbb051a6fb26f8eb0
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-26 15:32:48 +0200

    Update page.tsx

--- src/app/glp-1/page.tsx
+++ src/app/glp-1/page.tsx
@@ -7,58 +7,8 @@ export default function GLP1Page() {
       <Header />
 
       <main>
-
-        <section className="glpHero">
-
-          <div className="glpHeroContent">
-
-            <span className="glpEyebrow">
-              FORTEMATIC GLP-1
-            </span>
-
-            <h1>
-              Support your body's
-              <br />
-              natural GLP-1 pathways.
-            </h1>
-
-            <p>
-              A premium science-backed formula designed to
-              support appetite control, metabolic health and
-              everyday wellness.
-            </p>
-
-            <div className="glpButtons">
-
-              <a
-                href="#"
-                className="primaryButton"
-              >
-                Shop on Amazon
-              </a>
-
-              <a
-                href="#ingredients"
-                className="secondaryButton"
-              >
-                Learn How It Works
-              </a>
-
-            </div>
-
-          </div>
-
-          <div className="glpHeroImage">
-
-            {/* GLP-1 bottle goes here */}
-
-          </div>
-
-        </section>
-
+        <GLPHero />
       </main>
-
-  
     </>
   );
 }
