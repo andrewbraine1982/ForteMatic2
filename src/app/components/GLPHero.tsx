b30ee3c9080c0668e29d5f1aea23a7da83a5dd0b
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-26 15:08:30 +0200

    Create GLPHero.tsx

---
+++ src/app/components/GLPHero.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+export default function GLPHero() {
+  return (
+    <section className="glpHero">
+
+      <div className="glpHeroContent">
+
+        <span className="glpEyebrow">
+          FORTEMATIC GLP-1
+        </span>
+
+        <h1>
+          Support your body's
+          <br />
+          natural GLP-1 pathways.
+        </h1>
+
+        <p>
+          A premium science-backed formula designed to
+          support appetite control, metabolic health and
+          everyday wellness.
+        </p>
+
+        <div className="glpButtons">
+
+          <a
+            href="#"
+            className="primaryButton"
+          >
+            Shop on Amazon
+          </a>
+
+          <a
+            href="#ingredients"
+            className="secondaryButton"
+          >
+            Learn How It Works
+          </a>
+
+        </div>
+
+      </div>
+
+      <div className="glpHeroImage">
+
+        {/* GLP-1 bottle image goes here */}
+
+      </div>
+
+    </section>
+  );
+}
