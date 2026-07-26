58554e79e772ea738c62738198fa169a7703d338
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-26 15:08:59 +0200

    Update page.tsx

--- src/app/glp-1/page.tsx
+++ src/app/glp-1/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import GLPHero from "../components/GLPHero";
 
 export default function GLP1Page() {
   return (
