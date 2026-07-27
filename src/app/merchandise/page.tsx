f490bbb01067ebde9d9ff1712e2f21143c348863
andrewbraine1982 <andrewbraine@gmail.com>
2026-07-27 20:08:28 +0200

    Update page.tsx

--- src/app/merchandise/page.tsx
+++ src/app/merchandise/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Merchandise.module.css";
-import styles from "./Merchandise.module.css";
-import Link from "next/link";
 
 export default function MerchandisePage() {
   return (
