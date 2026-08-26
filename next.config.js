4c33132a6095dd48e04a8a553e760fc1d1622f09
andrewbraine1982 <andrewbraine@gmail.com>
2026-08-26 21:02:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/best-glp-1-supplement',
+        destination: 'https://fortematic-glp1-lander.vercel.app/',
+      },
+      {
+        source: '/best-glp-1-supplement/',
+        destination: 'https://fortematic-glp1-lander.vercel.app/',
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
