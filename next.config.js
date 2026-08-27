05166f607ff3b519b613f45aac984c1912378cd2
andrewbraine1982 <andrewbraine@gmail.com>
2026-08-27 13:34:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         source: '/best-glp-1-supplement/',
         destination: 'https://fortematic-glp1-lander.vercel.app/',
       },
+      {
+        source: '/best-glp-1-supplement/glp1-hero.png',
+        destination: 'https://fortematic-glp1-lander.vercel.app/glp1-hero.png',
+      },
     ]
   },
 }
